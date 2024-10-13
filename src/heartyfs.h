02447ec16239a7bb1e318895646e93a1f3ca575e
Mhonns <nsamaira@cmkl.ac.th>
2024-10-13 17:23:37 +0000

    Add the superblock directory and entry

--- src/heartyfs.h
+++ src/heartyfs.h
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <stdint.h>
+#include <unistd.h>
 #include <string.h>
 
 #define DISK_FILE_PATH "/tmp/heartyfs"
@@ -9,26 +11,69 @@
 #define DISK_SIZE (1 << 20)
 #define NUM_BLOCK (DISK_SIZE / BLOCK_SIZE)
 
-struct heartyfs_dir_entry {
+struct heartyfs_dir_entry 
+{
     int block_id;           // 4 bytes
     char file_name[28];     // 28 bytes
-};  // Overall: 32 bytes
+};  // Overall: 32 bytes 
 
-struct heartyfs_directory {
+struct heartyfs_directory 
+{
     int type;               // 4 bytes
     char name[28];          // 28 bytes
     int size;               // 4 bytes
-    struct heartyfs_dir_entry entries[14];  // 448 bytes
+    struct heartyfs_dir_entry entries[14]; // 448 bytes
 }; // Overall: 484 bytes
 
-struct heartyfs_inode {
+struct heartyfs_superblock 
+{
+    int total_blocks;       // 4 bytes
+    int free_blocks;        // 4 bytes
+    int block_size;         // 4 bytes
+    int type;               // 4 bytes
+    int size;               // 4 bytes
+    struct heartyfs_dir_entry entry; // 32 bytes
+}; // Overall: 52 bytes
+
+struct heartyfs_inode 
+{
     int type;               // 4 bytes
     char name[28];          // 28 bytes
     int size;               // 4 bytes
     int data_blocks[119];   // 476 bytes
 };  // Overall: 512 bytes
 
-struct heartyfs_data_block {
+struct heartyfs_data_block 
+{
     int size;               // 4 bytes
     char name[508];         // 508 bytes
 };  // Overall: 512 bytes
+
+extern uint8_t bitmap[NUM_BLOCK / 8];  // Bitmap with 2048 bits
+
+void free_block(uint8_t *bitmap, int block_id) 
+{
+    bitmap[block_id / 8] |= (1 << (block_id % 8));
+}
+
+void occupy_block(uint8_t *bitmap, int block_id) 
+{
+    bitmap[block_id / 8] &= ~(1 << (block_id % 8));
+}
+
+int status_block(uint8_t *bitmap, int block_id) 
+{
+    return (bitmap[block_id / 8] >> (block_id % 8)) & 1;
+}
+
+int find_free_block(uint8_t *bitmap) 
+{
+    for (int i = 0; i < NUM_BLOCK; i++) 
+    {
+        if (status_block(bitmap, i) > 0) 
+        {
+            return i;
+        }
+    }
+    return -1; // No free block found
+}
