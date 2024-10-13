da165403884ff1fc8ffe6794cfe8d54a33fc38c3
Mhonns <nsamaira@cmkl.ac.th>
2024-10-13 07:29:21 +0000

    Add superblock and bitmap block

--- src/heartyfs_init.c
+++ src/heartyfs_init.c
@@ -1,21 +1,5 @@
 #include "heartyfs.h"
 
-
-
-__uint8_t bitmap[NUM_BLOCK / 8];  // bitmap with 2048 bits > 256 bytes
-
-void free_block(int block_id) {
-    bitmap[block_id / 8] |= (1 << (block_id % 8));
-}
-
-void occupy_block(int block_id) {
-    bitmap[block_id / 8] &= ~(1 << (block_id % 8));
-}
-
-int status_block(int block_id) {
-    return (bitmap[block_id / 8] >> (block_id % 8)) & 1;
-}
-
 int main() {
     // Open the disk file
     int fd = open(DISK_FILE_PATH, O_RDWR);
@@ -32,20 +16,38 @@ int main() {
     }
 
     // TODO:
-    // TODO Delete this
-    printf("%d, %d, %d\n", BLOCK_SIZE, DISK_SIZE, NUM_BLOCK);
-    printf("\n buffer %p", buffer);
-
     // Initialize the superblock
-    struct heartyfs_directory root_dir;
-    root_dir.type = 1;
+    struct heartyfs_directory *root_dir = (struct heartyfs_directory *)(buffer);
+    root_dir->type = 1;
+    snprintf(root_dir->name, sizeof(root_dir->name), "%s", "root");
+    root_dir->size = 2;
+
+    root_dir->entries[0].block_id = 0;
+    snprintf(root_dir->entries[0].file_name, sizeof(root_dir->entries[0].file_name), "%s", ".");
+
+    root_dir->entries[1].block_id = 1;
+    snprintf(root_dir->entries[1].file_name, sizeof(root_dir->entries[1].file_name), "%s", "..");
 
     // Initialize the bitmap
-    memset(bitmap, 0xFF, sizeof(bitmap)); // Set all bits to 1
+    uint8_t bitmap[NUM_BLOCK / 8];          // bitmap with 2048 bits > 256 bytes
+    memset(bitmap, 0xFF, sizeof(bitmap));   // Set all bits to 1
 
     // Mark occupied
-    occupy_block(0);   // Occupied first block for super block
-    occupy_block(1);   // Occupied second block for bitmap
+    occupy_block(bitmap, 0);   // Occupied first block for superblock
+    occupy_block(bitmap, 1);   // Occupied second block for bitmap
+    memcpy((uint8_t *)(buffer + BLOCK_SIZE), bitmap, sizeof(bitmap)); // Put in the second block
 
+    // Test the block 1 and block 2
+    // TODO delete this
+    // struct heartyfs_directory *test = (struct heartyfs_directory *)(buffer);
+    // printf("\n %s", test->name);
+
+    // uint8_t *test2 = (uint8_t *)(buffer + BLOCK_SIZE);
+    // printf("\n %d\n", status_block(test2, 0));
+
+    // Clean up
+    close(fd);
+    munmap(buffer, DISK_SIZE);
+    
     return 0;
 }
