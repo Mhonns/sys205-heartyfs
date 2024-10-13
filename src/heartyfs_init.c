45f142931224c0031e9e5f274ca9f9aaaaf3f044
Mhonns <nsamaira@cmkl.ac.th>
2024-10-13 17:48:05 +0000

    add . and .. dir

--- src/heartyfs_init.c
+++ src/heartyfs_init.c
@@ -26,8 +26,12 @@ int main()
     superblock->block_size = BLOCK_SIZE;
     superblock->type = 1;
     superblock->size = 0;
-    superblock->entry.block_id = 0;
-    snprintf(superblock->entry.file_name, sizeof(superblock->entry.file_name), "%s", "root");
+
+    snprintf(superblock->dir_name, sizeof(superblock->dir_name), "%s", "/");
+    superblock->entries[1].block_id = 0;
+    snprintf(superblock->entries[0].file_name, sizeof(superblock->entries[0].file_name), "%s", ".");
+    superblock->entries[2].block_id = 0;
+    snprintf(superblock->entries[1].file_name, sizeof(superblock->entries[1].file_name), "%s", "..");
 
     // Initialize the bitmap
     uint8_t bitmap[NUM_BLOCK / 8];          // bitmap with 2048 bits > 256 bytes
@@ -38,13 +42,15 @@ int main()
     occupy_block(bitmap, 1);   // Occupied second block for bitmap
     memcpy((uint8_t *)(buffer + BLOCK_SIZE), bitmap, sizeof(bitmap)); // Put in the second block
 
-    // // Test the block 1 and block 2
-    // // TODO delete this
-    // struct heartyfs_directory *test = (struct heartyfs_directory *)(buffer);
-    // printf("\n %s", test->entries[0].name);
+    // Test the block 1 and block 2
+    // TODO delete this
+    struct heartyfs_superblock *test = (struct heartyfs_superblock *)(buffer);
+    printf("\n %s", test->dir_name);
+    printf("\n %s", test->entries[0].file_name);
+    printf("\n %s", test->entries[1].file_name);
 
-    // uint8_t *test2 = (uint8_t *)(buffer + BLOCK_SIZE);
-    // printf("\n %d\n", status_block(test2, 0));
+    uint8_t *test2 = (uint8_t *)(buffer + BLOCK_SIZE);
+    printf("\n %d\n", status_block(test2, 0));
 
     // Clean up
     msync(buffer, DISK_SIZE, MS_SYNC);
