3381acff03cc35d2ce96975e139aef1c7610b451
root <root@ssfs-01.apex.cmkl.ac.th>
2024-10-18 13:39:50 +0000

    Add the header comments to all functions

--- src/heartyfs_ops.c
+++ src/heartyfs_ops.c
@@ -1,20 +1,65 @@
+/*
+ * heartyfs_ops.c
+ * 
+ * Brief
+ * - This program provides helper operations for managing filesystem blocks, directories, and entries.
+ *   These utilities include block management, directory string checks, and operations for creating,
+ *   removing, and searching entries within the filesystem.
+ * 
+ * Data Structures:
+ * - Uses bitmaps to manage the status of blocks (free or occupied).
+ * - Directories and inodes are used to structure and organize the filesystem's files.
+ * 
+ * Design Decisions:
+ * - Functions are designed to interact directly with the memory-mapped filesystem, optimizing speed
+ *   and efficiency for filesystem manipulation.
+ * 
+ *                                          Created by Nathadon Samairat 18 Oct 2024
+ */
+
 #include "heartyfs.h"
 
+/*
+ * @brief Marks the specified block as free in the bitmap.
+ * 
+ * @param block_id      The ID of the block to free.
+ * @param bitmap        The bitmap tracking the status of blocks.
+ */
 void free_block(int block_id, uint8_t *bitmap) 
 {
     bitmap[block_id / 8] |= (1 << (block_id % 8));
 }
 
+/*
+ * @brief Marks the specified block as occupied in the bitmap.
+ * 
+ * @param block_id      The ID of the block to occupy.
+ * @param bitmap        The bitmap tracking the status of blocks.
+ */
 void occupy_block(int block_id, uint8_t *bitmap) 
 {
     bitmap[block_id / 8] &= ~(1 << (block_id % 8));
 }
 
+/*
+ * @brief Checks the status of the specified block in the bitmap.
+ * 
+ * @param block_id      The ID of the block to check.
+ * @param bitmap        The bitmap tracking the status of blocks.
+ * 
+ * @return int          1 if the block is free, 0 if occupied.
+ */
 int status_block(int block_id, uint8_t *bitmap) 
 {
     return (bitmap[block_id / 8] >> (block_id % 8)) & 1;
 }
 
+/*
+ * @brief Searches for a free block in the bitmap.
+ *
+ * @param bitmap        The bitmap tracking the status of blocks.
+ * @return int          The ID of the first free block found, or -1 if no free block is available.
+ */
 int find_free_block(uint8_t *bitmap) 
 {
     for (int i = 0; i < NUM_BLOCK; i++) 
@@ -27,6 +72,22 @@ int find_free_block(uint8_t *bitmap)
     return -1; // No free block found
 }
 
+/*
+ * @brief -Parses the input directory string to verify the path and update the parent directory.
+ *         It will also return the parent directory of the given string that match with the current structure
+ * 
+ * @param input_str         The directory path string to parse.
+ * @param dir_name          The output name of the directory being checked.
+ * @param buffer            The memory-mapped buffer of the disk image.
+ * @param parent_dir        Pointer to the parent directory structure.
+ * @param bitmap            bitmap tracking the status of blocks.
+ * @return * int            The difference between the depth of the path and the matched depth.
+ * 
+ * eg.  input_str: /dir1/dir2, current structure /dir1/dir3 will 
+ *          return 1 and the parent_dir is dir1
+ *      input_str: /dir1/dir3, current structure /dir1/dir3 will
+ *          return 0 and the parent_dir is dir3
+ */
 int dir_string_check(char *input_str, char *dir_name, void* buffer,
                         struct heartyfs_directory **parent_dir, uint8_t *bitmap)
 {
@@ -46,8 +107,8 @@ int dir_string_check(char *input_str, char *dir_name, void* buffer,
                 if (temp_dir->type == 1)    // check whether it is a directory or not
                 {
                     *parent_dir = (struct heartyfs_directory *) (buffer + BLOCK_SIZE * parent_block_id);
+                    matched_depth++;
                 }
-                matched_depth++;
             }
             else if (parent_block_id == 0)
             {
@@ -64,6 +125,13 @@ int dir_string_check(char *input_str, char *dir_name, void* buffer,
     return diff;
 }
 
+/*
+ * @brief Searches for an entry with the specified name in the given directory.
+ * 
+ * @param parent_dir    The directory structure to search in.
+ * @param target_name   The name of the entry to search for.
+ * @return int          The block ID of the found entry, or -1 if not found.
+ */
 int search_entry_in_dir(struct heartyfs_directory *parent_dir, char *target_name)
 {
     for (int i = 0; i < parent_dir->size; i++)
@@ -76,6 +144,16 @@ int search_entry_in_dir(struct heartyfs_directory *parent_dir, char *target_name
     return -1;
 }
 
+/*
+ * @brief Creates a new entry in the specified parent directory.
+ * 
+ * @param superblock            The superblock structure of the filesystem.
+ * @param parent_dir            The directory structure where the entry will be created.
+ * @param target_name           The name of the entry to create.
+ * @param target_block_id       The block ID assigned to the entry.
+ * @param bitmap                The bitmap tracking the status of blocks.
+ * @return int                  1 on success, -1 if the directory is full.
+ */
 int create_entry(struct heartyfs_superblock *superblock, struct heartyfs_directory *parent_dir, 
                     char *target_name, int target_block_id, uint8_t *bitmap)
 {
@@ -97,6 +175,15 @@ int create_entry(struct heartyfs_superblock *superblock, struct heartyfs_directo
     }
 }
 
+/*
+ * @brief Removes the specified entry from a parent directory.
+ * 
+ * @param superblock            The superblock structure of the filesystem.
+ * @param buffer                The memory-mapped buffer of the disk image.
+ * @param parent_block_id       The block ID of the parent directory.
+ * @param target_name           The name of the entry to remove.
+ * @return int                  - 1 on success, -1 if the entry is not found or if the removal is denied.
+ */
 int remove_entry(struct heartyfs_superblock *superblock, void* buffer, 
                     int parent_block_id, char *target_name)
 {
@@ -126,6 +213,12 @@ int remove_entry(struct heartyfs_superblock *superblock, void* buffer,
     }
 }
 
+/*
+ * @brief Cleans up resources, synchronizing and unmapping the buffer, and closing the file.
+ * 
+ * @param buffer        The memory-mapped buffer of the disk image.
+ * @param fd            The file descriptor of the disk image.
+ */
 void cleanup(void *buffer, int fd) 
 {
     if (buffer != NULL) {
